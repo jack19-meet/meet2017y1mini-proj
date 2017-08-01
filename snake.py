ff1ed094503067b2a0a402005b922723cabea8c8
jack19-meet <jack19@meet.mit.edu>
2017-08-01 14:26:06 +0300

    Stopping here

--- snake.py
+++ snake.py
@@ -47,28 +47,29 @@ DOWN = 2
 RIGHT = 3
 direction = UP
 
+UP_EDGE = 250
+DOWN_EDGE = -250
+RIGHT_EDGE = 400
+LEFT_EDGE = -400
+
 def up ():
     global direction
     direction = UP
-    move_snake()
     print('you pressed the Up key!')
 
 def left ():
     global direction
     direction = LEFT
-    move_snake()
     print('you pressed the Left key!')
 
 def down ():
     global direction
     direction = DOWN
-    move_snake()
     print('you pressed the Down key!')
 
 def right ():
     global direction
     direction = RIGHT
-    move_snake()
     print('you pressed the Right key!')
 
 turtle.onkeypress(up,UP_ARROW)
@@ -78,6 +79,7 @@ turtle.onkeypress(right,RIGHT_ARROW)
 turtle.listen()
 
 def move_snake():
+    global direction
     my_pos = snake.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
@@ -95,16 +97,48 @@ def move_snake():
         snake.goto(x_pos,y_pos-SQUARE_SIZE)
         print('you moved down')
 
+    new_pos = snake.pos()
+    new_x_pos = new_pos[0]
+    new_y_pos = new_pos[1]
+    if new_x_pos>=RIGHT_EDGE:
+        print('ypu hit the right edge!game over')
+        quit()
+    elif new_x_pos<=LEFT_EDGE:
+        print('you hit the left edge!game over')
+        quit()
+    elif new_y_pos>=UP_EDGE:
+        print('you hit the up edge!game over')
+        quit()
+    elif new_y_pos<=DOWN_EDGE:
+        print('you hit the down edge!game over')
+    turtle.ontimer(move_snake,TIME_STEP)
+    
     my_pos = snake.pos()
     pos_list.append(my_pos)
     new_stamp = snake.stamp()
     stamp_list.append(new_stamp)
-    old_stamp = stamp_list.pop()
+    global food_stamp, food_pos
+    if snake.pos() in food_pos:
+        food_ind = food_pos.index(snake.pos())
+        food.clearstamp(food_stamps[food_ind])
+
+        food_pos.pop(food_ind)
+        food_stamps.pop(food_ind)
+        print('you have eaten the food!')
+        
+    old_stamp = stamp_list.pop(0)
     snake.clearstamp(old_stamp)
     pos_list.pop(0)
 
-    
-    
+move_snake()
 
+turtle.register_shape("trash.gif")
+food = turtle.clone()
+food.shape("trash.gif")
+food_pos = [(100,100), (-100,100), (-100,-100), (100,-100)]
+food_stamp = []
 
-
+for this_food_pos in food_pos:
+    food.goto(this_food_pos[0],this_food_pos[1])
+    food.stamp()
+ 
